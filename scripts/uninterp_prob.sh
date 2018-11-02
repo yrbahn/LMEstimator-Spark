347fa96ce4f33e481f03d87a32c28fd1a7e2dfe4
Youngrok Bahn <yrbahn@gmail.com>
2018-11-02 21:56:31 +0900

    Update uninterp_prob.sh

--- scripts/uninterp_prob.sh
+++ scripts/uninterp_prob.sh
@@ -6,4 +6,4 @@ spark-submit --master spark://search-voice-spark1.dakao.io:6066 \
 --executor-cores 3 \
 --conf spark.hadoop.validateOutputSpecs=false \
 --conf spark.network.timeout=2000s \
-http://dict6.dialoid.com/sparklm-assembly-0.1.jar --command uninterp_prob --config http://dict6.dialoid.com/application.conf  
+http://xxx/sparklm-assembly-0.1.jar --command uninterp_prob --config http://xxx/application.conf  
